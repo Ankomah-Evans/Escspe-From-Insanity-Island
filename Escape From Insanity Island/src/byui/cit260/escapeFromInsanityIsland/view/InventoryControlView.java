592cfb6c20996073405f3ad74069ac4ed4921218
user <user@ANKOMAH>
2016-06-08 11:11:08 -0600

    7/6/2016 MainMenuView and GameMenuView created

--- Escape From Insanity Island/src/byui/cit260/escapeFromInsanityIsland/view/InventoryControlView.java
+++ Escape From Insanity Island/src/byui/cit260/escapeFromInsanityIsland/view/InventoryControlView.java
@@ -5,9 +5,6 @@
  */
 package byui.cit260.escapeFromInsanityIsland.view;
 
-import buyi.cit260.escapeFromInsanityIsland.control.sizeOfArrow;
-import escape.from.insanity.island.EscapeFromInsanityIsland;
-
 /**
  *
  * @author user
@@ -19,14 +16,7 @@ public class InventoryControlView {
 
   public  void displayInventoryControlView() {
       
-      //  create a new arrow
-      int value = sizeOfArrow.createNewArrow(EscapeFromInsanityIsland.getArrow());
-      if ( value < 10 && value <4){
-          System.out.println();
-      }
-      // display inventory control
-      InventoryControlView inventoryControlView = new InventoryControlView();
-        inventoryControlView.displayInventoryControlView();
+      System.out.println("Stub function called");
         
     }
     
