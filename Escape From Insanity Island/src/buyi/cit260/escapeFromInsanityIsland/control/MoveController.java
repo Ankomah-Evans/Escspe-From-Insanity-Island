8cfc02e488413e27537577962bfb7a52b3fd4637
StarrfoxxW8 <StarrfoxxW8@192.168.65.132>
2016-06-30 21:11:58 -0600

    .

---
+++ Escape From Insanity Island/src/buyi/cit260/escapeFromInsanityIsland/control/MoveController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buyi.cit260.escapeFromInsanityIsland.control;
+
+/**
+ *
+ * @author StarrfoxxW8
+ */
+public class MoveController {
+    
+    public boolean moveNorth (game game)
+            
+            Character character = game.getCharacter();
+            Location currentLocation = game.getCharacter().getLocation();
+            Map map = game.getMap();
+            
+            if (currentLocation.getRow() == 0 {
+                return false;
+            }
+            
+            
+            
+            
+}
