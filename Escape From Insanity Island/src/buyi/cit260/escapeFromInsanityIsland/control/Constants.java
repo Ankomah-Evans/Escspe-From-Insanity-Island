48e006d63c837e16ec6a035742976548101ecfff
StarrfoxxW8 <StarrfoxxW8@192.168.65.132>
2016-07-19 00:07:42 -0600

    Added an array to WorldResources.

---
+++ Escape From Insanity Island/src/buyi/cit260/escapeFromInsanityIsland/control/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buyi.cit260.escapeFromInsanityIsland.control;
+
+/**
+ *
+ * @author StarrfoxxW8
+ */
+class Constants {
+
+    static int NUMBER_OF_MATERIALS;
+    static int IRON;
+    static int GOLD;
+    static int STONE;
+    static int WOOD;
+    static int CLOTH;
+    static int LEATHER;
+    
+}
--- Escape From Insanity Island/src/buyi/cit260/escapeFromInsanityIsland/control/WorldResources.java
+++ Escape From Insanity Island/src/buyi/cit260/escapeFromInsanityIsland/control/WorldResources.java
@@ -10,6 +10,32 @@ package buyi.cit260.escapeFromInsanityIsland.control;
  * @author StarrfoxxW8
  */
 public class WorldResources {
+    
+   private static materials[] createMaterialList() {
+       
+       // create list of resource materials
+       Material[] materials = new Material[Constants.NUMBER_OF_MATERIALS];
+       
+       Material iron = new Material("iron");
+       materials[Constants.IRON] = iron;
+       
+       Material gold = new Material("gold");
+       materials[Constants.GOLD] = gold;
+       
+       Material stone = new Material("stone");
+       materials[Constants.STONE] = stone;
+       
+       Material wood = new Material("wood");
+       materials[Constants.WOOD] = wood;
+       
+       Material cloth = new Material("cloth");
+       materials[Constants.CLOTH] = cloth;
+       
+       Material leather = new Material("leather");
+       materials[Constants.LEATHER] = leather;
+       
+       return materials;
+   }
 
     void setDescription(String iron) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
